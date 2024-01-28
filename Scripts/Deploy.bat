13a5279e0f8aa35b7e203bf059fde961a1121fb7
Pierre Van dhelsen <vandhelsen.pierre@gmail.com>
2024-01-28 17:11:24 -0500

    [Project] Deploy script

---
+++ Scripts/Deploy.bat
@@ -0,0 +1,23 @@
+@echo off
+
+SET destination=%1
+SET version=%2
+
+if [%destination%]==[] (SET /p destination="Enter deploy destination folder: ")
+if [%version%]==[] (SET /p version="Enter version: ")
+
+SET install=%destination%\Install\
+SET archive=%destination%\%version%.zip
+
+rmdir /s /q %install%
+del %archive%
+
+robocopy ../builds %install%
+robocopy ../Assets %install%/Assets /e
+robocopy ../Config %install%/Config /e
+robocopy ../Scripts/Premake %install%/Scripts/Premake /e
+robocopy ../Sources %install%/Sources /e /xf **.vcxproj *vcxproj.user
+
+powershell Compress-Archive %install% %archive%
+
+pause
