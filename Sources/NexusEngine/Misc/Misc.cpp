0e48e7780868f16c9807bab4f6f86d85d3df7448
Pierre Van dhelsen <vandhelsen.pierre@gmail.com>
2024-04-18 00:26:24 -0400

    [Misc] Modulo

--- Sources/NexusEngine/Misc/Misc.cpp
+++ Sources/NexusEngine/Misc/Misc.cpp
@@ -17,4 +17,49 @@ namespace NxEn
 		NEXUS_BITSCANFORWARD(Number, Index, Result)
 		return Result > 0 ? Index : -1;
 	}
+
+#define MODULO(Type, Number, Mode)\
+	NEXUS_ASSERT(Mod != 0, "Modulo with Mode == 0 is undefined");\
+	Type Remainder = Number % Mod;\
+	return Remainder < 0 ? Remainder + Mod : Remainder;
+
+	int8 Modulo(int8 Number, int8 Mod)
+	{
+		MODULO(int8, Number, Mode);
+	}
+
+	int16 Modulo(int16 Number, int16 Mod)
+	{
+		MODULO(int16, Number, Mode);
+	}
+
+	int32 Modulo(int32 Number, int32 Mod)
+	{
+		MODULO(int32, Number, Mode);
+	}
+
+	int64 Modulo(int64 Number, int64 Mod)
+	{
+		MODULO(int64, Number, Mode);
+	}
+
+	uint8 Modulo(uint8 Number, uint8 Mod)
+	{
+		MODULO(uint8, Number, Mode);
+	}
+
+	uint16 Modulo(uint16 Number, uint16 Mod)
+	{
+		MODULO(uint16, Number, Mode);
+	}
+	
+	uint32 Modulo(uint32 Number, uint32 Mod)
+	{
+		MODULO(uint32, Number, Mode);
+	}
+
+	uint64 Modulo(uint64 Number, uint64 Mod)
+	{
+		MODULO(uint64, Number, Mode);
+	}
 }
--- Sources/NexusEngine/Misc/Misc.h
+++ Sources/NexusEngine/Misc/Misc.h
@@ -8,4 +8,13 @@ namespace NxEn
 
 	NEXUS_ENGINE_API bool IsPowerOfTwo(uint64 Number);
 	NEXUS_ENGINE_API int32 LogTwoPowerOfTwo(uint32 Number);
+
+	NEXUS_ENGINE_API int8 Modulo(int8 Number, int8 Mod);
+	NEXUS_ENGINE_API int16 Modulo(int16 Number, int16 Mod);
+	NEXUS_ENGINE_API int32 Modulo(int32 Number, int32 Mod);
+	NEXUS_ENGINE_API int64 Modulo(int64 Number, int64 Mod);
+	NEXUS_ENGINE_API uint8 Modulo(uint8 Number, uint8 Mod);
+	NEXUS_ENGINE_API uint16 Modulo(uint16 Number, uint16 Mod);
+	NEXUS_ENGINE_API uint32 Modulo(uint32 Number, uint32 Mod);
+	NEXUS_ENGINE_API uint64 Modulo(uint64 Number, uint64 Mod);
 }
