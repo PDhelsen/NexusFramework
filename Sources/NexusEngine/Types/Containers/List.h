9c005f89570f04f19535ecf81c48ec579ba13bf7
Pierre Van dhelsen <vandhelsen.pierre@gmail.com>
2024-04-20 19:28:33 -0400

    [Container] List

---
+++ Sources/NexusEngine/Types/Containers/List.h
@@ -0,0 +1,399 @@
+#pragma once
+
+#include "Types/Integer.h"
+#include "Memory/Memory.h"
+#include "Memory/Allocator/Allocator.h"
+#include "Debug/Assert.h"
+#include "Misc/Sort.h"
+#include "Misc/References.h"
+
+namespace NxEn
+{
+	template<typename T>
+	class List
+	{
+	public:
+		class Iterator
+		{
+		public:
+			Iterator(T* Ptr)
+				: Pointer(Ptr)
+			{
+
+			}
+
+			Iterator& operator++()
+			{
+				Pointer++;
+				return *this;
+			}
+
+			Iterator operator++(int32)
+			{
+				Iterator Copy = *this;
+				++(*this);
+				return Copy;
+			}
+
+			Iterator& operator--()
+			{
+				Pointer--;
+				return *this;
+			}
+
+			Iterator operator--(int32)
+			{
+				Iterator Copy = *this;
+				--(*this);
+				return Copy;
+			}
+
+			T* operator->()
+			{
+				return Pointer;
+			}
+
+			T& operator*()
+			{
+				return *Pointer;
+			}
+
+			bool operator==(const Iterator& Other) const
+			{
+				return Pointer == Other.Pointer;
+			}
+
+			bool operator!=(const Iterator& Other) const
+			{
+				return Pointer != Other.Pointer;
+			}
+
+		private:
+			T* Pointer;
+		};
+
+		List(uint64 Cpct = 2, Allocator* Alloc = nullptr)
+			: Allocator(nullptr), Capacity(0), Count(0), Data(nullptr)
+		{
+			Allocator = Alloc != nullptr ? Alloc : Memory::GetActiveAllocator();
+			Capacity = GetValidCapacity(Cpct);
+			Data = (T*)Memory::Allocate(sizeof(T) * Capacity, NEXUS_MEMORY_ALIGN, Allocator);
+		}
+
+		List(const List<T>& Other)
+			: Allocator(Other.Allocator), Capacity(Other.Capacity), Count(Other.Count), Data(Other.Data)
+		{
+		}
+
+		List(List<T>&& Other) noexcept
+			: Allocator(Other.Allocator), Capacity(Other.Capacity), Count(Other.Count), Data(Other.Data)
+		{
+			Other.Data = nullptr;
+		}
+
+		~List()
+		{
+			Memory::Free(Data, Allocator);
+		}
+
+		List<T> Copy() const
+		{
+			List<T> Copy = List<T>(Capacity, Allocator);
+			Copy.Count = Count;
+			for (uint64 Index = 0; Index < Count; Index++)
+			{
+				Copy.Data[Index] = Data[Index];
+			}
+			return Copy;
+		}
+
+		T& operator[](uint64 Index)
+		{
+			NEXUS_ASSERT(IsValidIndex(Index), "Invalid Index");
+			return Data[Index];
+		}
+
+		const T& operator[](uint64 Index) const
+		{
+			NEXUS_ASSERT(IsValidIndex(Index), "Invalid Index");
+			return Data[Index];
+		}
+
+		bool operator==(const List<T>& Other)
+		{
+			return Count == Other.Count && Data == Other.Data;
+		}
+
+		bool operator!=(const List<T>& Other)
+		{
+			return Count != Other.Count || Data != Other.Data;
+		}
+
+		void Assign(uint64 Index, const T& Value)
+		{
+			NEXUS_ASSERT(IsValidIndex(Index), "Invalid Index");
+			Data[Index] = Value;
+		}
+
+		void Assign(uint64 Index, T&& Value)
+		{
+			NEXUS_ASSERT(IsValidIndex(Index), "Invalid Index");
+			Data[Index] = Move(Value);
+		}
+
+		template<typename... Args>
+		void AssignConstruct(uint64 Index, Args&&... args)
+		{
+			NEXUS_ASSERT(IsValidIndex(Index), "Invalid Index");
+			Data[Index] = T(args...);
+		}
+
+		void AssignRange(uint64 Index, List<T>& Values)
+		{
+			NEXUS_ASSERT(IsValidIndex(Index), "Invalid Index");
+			NEXUS_ASSERT(IsValidIndex(Index + Values.GetCount() - 1), "Invalid Index");
+
+			for (uint64 Offset = 0; Offset < Values.GetCount(); Offset++)
+			{
+				Data[Index + Offset] = Values[Offset];
+			}
+		}
+
+		void Append(const T& Value)
+		{
+			Resize(++Count);
+			Data[Count - 1] = Value;
+		}
+
+		void Append(T&& Value)
+		{
+			Resize(++Count);
+			Data[Count - 1] = Move(Value);
+		}
+
+		template<typename... Args>
+		void AppendConstruct(Args&&... args)
+		{
+			Resize(++Count);
+			Data[Count - 1] = T(args...);
+		}
+
+		void AppendRange(const List<T>& Values)
+		{
+			uint64 Index = GetCount();
+			Resize(GetCount() + Values.GetCount());
+			for (uint64 Offset = 0; Offset < Values.GetCount(); Offset++)
+			{
+				Data[Index + Offset] = Values[Offset];
+			}
+		}
+
+		void Insert(uint64 Index, const T& Value)
+		{
+			NEXUS_ASSERT(IsValidIndex(Index), "Invalid Index");
+
+			Resize(++Count);
+			Memory::MemCopy(&Data[Index], &Data[Index + 1], sizeof(T) * (Count - Index - 1));
+			Data[Index] = Value;
+		}
+
+		void Insert(uint64 Index, T&& Value)
+		{
+			NEXUS_ASSERT(IsValidIndex(Index), "Invalid Index");
+
+			Resize(++Count);
+			Memory::MemCopy(&Data[Index], &Data[Index + 1], sizeof(T) * (Count - Index - 1));
+			Data[Index] = Move(Value);
+		}
+
+		template<typename... Args>
+		void InsertConstruct(uint64 Index, Args&&... args)
+		{
+			NEXUS_ASSERT(IsValidIndex(Index), "Invalid Index");
+
+			Resize(++Count);
+			Memory::MemCopy(&Data[Index], &Data[Index + 1], sizeof(T) * (Count - Index - 1));
+			Data[Index] = T(args...);
+		}
+
+		void InsertRange(uint64 Index, const List<T>& Values)
+		{
+			NEXUS_ASSERT(IsValidIndex(Index), "Invalid Index");
+
+			Resize(GetCount() + Values.GetCount());
+			Memory::MemCopy(&Data[Index], &Data[Index + Values.GetCount()], sizeof(T) * (Count - Index - Values.GetCount()));
+			for (uint64 Offset = 0; Offset < Values.GetCount(); Offset++)
+			{
+				Data[Index + Offset] = Values[Offset];
+			}
+		}
+
+		void Remove(uint64 Index)
+		{
+			Memory::MemCopy(&Data[Index + 1], &Data[Index], sizeof(T) * (Count - Index - 1));
+			Resize(--Count);
+		}
+
+		void RemoveRange(uint64 Index, uint64 Size)
+		{
+			Memory::MemCopy(&Data[Index + Size], &Data[Index], sizeof(T) * (Count - Index - Size));
+			Resize(Count - Size);
+		}
+
+		void RemoveLast()
+		{
+			Resize(--Count);
+		}
+
+		void RemoveSwap(uint64 Index)
+		{
+			Swap(Index, Count - 1);
+			RemoveLast();
+		}
+
+		void Clear(bool ShrinkToZero = false)
+		{
+			Resize(0);
+			if (ShrinkToZero)
+			{
+				Shrink();
+			}
+		}
+
+		T& Get(uint64 Index)
+		{
+			NEXUS_ASSERT(IsValidIndex(Index), "Invalid Index");
+			return Data[Index];
+		}
+
+		const T& Get(uint64 Index) const
+		{
+			NEXUS_ASSERT(IsValidIndex(Index), "Invalid Index");
+			return Data[Index];
+		}
+
+		T& First()
+		{
+			return Data[0];
+		}
+
+		T& Last()
+		{
+			return Data[Count - 1];
+		}
+
+		void Swap(uint64 IndexA, uint64 IndexB)
+		{
+			NEXUS_ASSERT(IsValidIndex(IndexA), "Invalid Index");
+			NEXUS_ASSERT(IsValidIndex(IndexB), "Invalid Index");
+
+			T Temp = Data[IndexB];
+			Data[IndexB] = Move(Data[IndexA]);
+			Data[IndexA] = Move(Temp);
+		}
+
+		Iterator begin() const { return Begin(); }
+		Iterator Begin() const
+		{
+			return Iterator(Data);
+		}
+
+		Iterator BeginReverse() const
+		{
+			return --End();
+		}
+
+		Iterator end() const { return End(); }
+		Iterator End() const
+		{
+			return Iterator(Data + Count);
+		}
+
+		Iterator EndReverse() const
+		{
+			return --Begin();
+		}
+
+		bool IsValidIndex(uint64 Index) const
+		{
+			return Index >= 0 && Index < Count;
+		}
+
+		void Resize(uint64 Size)
+		{
+			Count = Size;
+
+			if (Count > Capacity)
+			{
+				uint64 NewCapacity = Capacity + Capacity / 2;
+				Capacity = GetValidCapacity(NewCapacity > Count ? NewCapacity : Count);
+
+				Data = (T*)Memory::Realloc(Data, sizeof(T) * Capacity, NEXUS_MEMORY_ALIGN, Allocator);
+			}
+
+			NEXUS_ASSERT(Count <= Capacity, "Overflowing list");
+		}
+
+		void Reserve(uint64 Size)
+		{
+			if (Size <= Capacity)
+			{
+				return;
+			}
+
+			Capacity = GetValidCapacity(Size);
+			Data = (T*)Memory::Realloc(Data, sizeof(T) * Capacity, NEXUS_MEMORY_ALIGN, Allocator);
+		}
+
+		void Shrink()
+		{
+			Capacity = GetValidCapacity(Count);
+			Data = (T*)Memory::Realloc(Data, sizeof(T) * Capacity, NEXUS_MEMORY_ALIGN, Allocator);
+		}
+
+		bool Contains(const T& Other) const
+		{
+			return Find(Other) < Count;
+		}
+
+		uint64 Find(const T& Other) const
+		{
+			for (uint64 Index = 0; Index < Count; Index++)
+			{
+				if (Data[Index] == Other)
+				{
+					return Index;
+				}
+			}
+
+			return Count;
+		}
+
+		void Sort()
+		{
+			Sort::HeapSort(Data, Count);
+		}
+
+		void Reverse()
+		{
+			uint64 Half = Count / 2;
+			for (uint64 Front = 0, Back = Count - 1; Front < Half; Front++, Back--)
+			{
+				Swap(Front, Back);
+			}
+		}
+
+		uint64 GetCount() const { return Count; }
+		uint64 GetCapacity() const { return Capacity; }
+		bool IsEmpty() const { return Count == 0; }
+		T* GetRaw() { return Data; }
+
+	private:
+		uint64 GetValidCapacity(uint64 Size) { return Size > 2 ? Size : 2; }
+
+		Allocator* Allocator;
+		uint64 Capacity;
+		uint64 Count;
+		T* Data;
+	};
+}
--- Sources/NexusTest/Engine/Types/Containers.cpp
+++ Sources/NexusTest/Engine/Types/Containers.cpp
@@ -1,6 +1,7 @@
 #include "Core/NexusTestPch.h"
 
 #include "Types/Containers/Array.h"
+#include "Types/Containers/List.h"
 
 namespace NxTs
 {
@@ -110,7 +111,7 @@ namespace NxTs
 		ASSERT_EQ(Test[6].Integer, 6);
 
 		Test.Assign(5, ContainerTest(7));
-		Test.Assign(7, 8);
+		Test.AssignConstruct(7, 8);
 		ASSERT_EQ(Test[5].Integer, 7);
 		ASSERT_EQ(Test[7].Integer, 8);
 
@@ -135,7 +136,7 @@ namespace NxTs
 		}
 		ASSERT_EQ(Test[3].Integer, 3);
 
-		Index = 8;
+		Index = Test.GetCount() - 1;
 		for (NxEn::Array<ContainerTest>::Iterator Iterator = Test.BeginReverse(); Iterator != Test.EndReverse(); --Iterator)
 		{
 			ContainerTest& Container = *Iterator;
@@ -169,6 +170,166 @@ namespace NxTs
 		Test[6].Integer = 6;
 		Test[7].Integer = 2;
 		Test[8].Integer = 3;
+
+		Test.Sort();
+		for (Index = 0; Index < Test.GetCount(); Index++)
+		{
+			ASSERT_EQ(Test[Index].Integer, Index);
+		}
+
+		Test.Reverse();
+		for (Index = 0; Index < Test.GetCount(); Index++)
+		{
+			ASSERT_EQ(Test[Index].Integer, Test.GetCount() - 1 - Index);
+		}
+	}
+
+	TEST(Type_Containers, List)
+	{
+		NxEn::List<ContainerTest> Test = NxEn::List<ContainerTest>(3);
+		ASSERT_EQ(Test.GetCapacity(), 3);
+		ASSERT_EQ(Test.GetCount(), 0);
+		ASSERT_EQ(Test.IsEmpty(), true);
+
+		Test.Append(ContainerTest(5));
+		Test.Append(ContainerTest(6));
+		Test.Append(ContainerTest(7));
+		Test.Append(ContainerTest(8));
+		Test.Append(ContainerTest(9));
+		ASSERT_EQ(Test.GetCapacity(), 6);
+		ASSERT_EQ(Test.GetCount(), 5);
+		ASSERT_EQ(Test.IsEmpty(), false);
+
+		NxEn::List<ContainerTest> CopyDeep = Test.Copy();
+		ASSERT_NE(&Test[0], &CopyDeep[0]);
+		ASSERT_EQ(Test[0].Integer, CopyDeep[0].Integer);
+		ASSERT_EQ(Test == CopyDeep, false);
+
+		ContainerTest Copy = Test[0];
+		Copy.Integer = 10;
+		ASSERT_NE(Test[0].Integer, 10);
+
+		ContainerTest& Reference = Test[0];
+		Reference.Integer = 10;
+		ASSERT_EQ(Test[0].Integer, 10);
+
+		Test[0] = ContainerTest(3);
+		Test[1] = ContainerTest(6);
+		ASSERT_EQ(Test[0].Integer, 3);
+		ASSERT_EQ(Test[1].Integer, 6);
+
+		Test.Assign(0, ContainerTest(7));
+		Test.AssignConstruct(1, 8);
+		ASSERT_EQ(Test[0].Integer, 7);
+		ASSERT_EQ(Test[1].Integer, 8);
+
+		Test.AssignRange(0, CopyDeep);
+		ASSERT_EQ(Test[0].Integer, 5);
+		ASSERT_EQ(Test[4].Integer, 9);
+
+		Test.Append(ContainerTest(12));
+		Test.AppendConstruct(11);
+		ASSERT_EQ(Test[5].Integer, 12);
+		ASSERT_EQ(Test[6].Integer, 11);
+
+		Test.AppendRange(CopyDeep);
+		ASSERT_EQ(Test[0].Integer, 5);
+		ASSERT_EQ(Test[11].Integer, 9);
+
+		Test.Insert(5, Copy);
+		ASSERT_EQ(Test[5].Integer, 10);
+		ASSERT_EQ(Test[6].Integer, 12);
+
+		Test.InsertConstruct(5, 13);
+		ASSERT_EQ(Test[5].Integer, 13);
+		ASSERT_EQ(Test[6].Integer, 10);
+
+		Test.InsertRange(5, CopyDeep);
+		ASSERT_EQ(Test[5].Integer, 5);
+		ASSERT_EQ(Test[9].Integer, 9);
+		ASSERT_EQ(Test[10].Integer, 13);
+
+		Test.Remove(5);
+		ASSERT_EQ(Test[5].Integer, 6);
+
+		Test.RemoveRange(5, 4);
+		ASSERT_EQ(Test[5].Integer, 13);
+
+		Test.RemoveLast();
+		ASSERT_EQ(Test[12].Integer, 8);
+
+		Test.RemoveSwap(5);
+		ASSERT_EQ(Test[5].Integer, 8);
+
+		Test.Clear(true);
+		ASSERT_EQ(Test.GetCount(), 0);
+		ASSERT_EQ(Test.GetCapacity(), 2);
+
+		Test.AppendRange(CopyDeep);
+
+		Test[0].Integer = 3;
+		Test[1].Integer = 6;
+		Test.Swap(0, 1);
+		ASSERT_EQ(Test[0].Integer, 6);
+		ASSERT_EQ(Test[1].Integer, 3);
+
+		uint64 Index = 0;
+		for (NxEn::List<ContainerTest>::Iterator Iterator = Test.Begin(); Iterator != Test.End(); ++Iterator)
+		{
+			ContainerTest& Container = *Iterator;
+			Container.Integer = Index;
+			Container.Float = (float)Index;
+			Container.Boolean = Index % 2 == 1;
+			Index++;
+		}
+		ASSERT_EQ(Test[3].Integer, 3);
+
+		Index = Test.GetCount() - 1;
+		for (NxEn::List<ContainerTest>::Iterator Iterator = Test.BeginReverse(); Iterator != Test.EndReverse(); --Iterator)
+		{
+			ContainerTest& Container = *Iterator;
+			Container.Integer = Index;
+			Container.Float = (float)Index;
+			Container.Boolean = Index % 2 == 1;
+			Index--;
+		}
+		ASSERT_EQ(Test[3].Integer, 3);
+
+		Index = 0;
+		for (ContainerTest& Container : Test)
+		{
+			Container.Integer = Index;
+			Container.Float = (float)Index;
+			Container.Boolean = Index % 2 == 1;
+			Index++;
+		}
+		ASSERT_EQ(Test[3].Integer, 3);
+
+		Test.Resize(9);
+		ASSERT_EQ(Test.GetCount(), 9);
+		ASSERT_EQ(Test.GetCapacity(), 9);
+
+		Test.Reserve(18);
+		ASSERT_EQ(Test.GetCount(), 9);
+		ASSERT_EQ(Test.GetCapacity(), 18);
+
+		Test.Shrink();
+		ASSERT_EQ(Test.GetCount(), 9);
+		ASSERT_EQ(Test.GetCapacity(), 9);
+
+		ContainerTest Test2 = ContainerTest(2);
+		ASSERT_EQ(Test.Contains(Test2), true);
+		ASSERT_EQ(Test.Find(Test2), 2);
+
+		Test[0].Integer = 3;
+		Test[1].Integer = 4;
+		Test[2].Integer = 2;
+		Test[3].Integer = 1;
+		Test[4].Integer = 6;
+		Test[5].Integer = 8;
+		Test[6].Integer = 5;
+		Test[7].Integer = 7;
+		Test[8].Integer = 0;
 
 		Test.Sort();
 		for (Index = 0; Index < Test.GetCount(); Index++)
