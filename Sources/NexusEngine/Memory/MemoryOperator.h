c5076cb35f5c829ca10c20a48f69d531aaf035d2
Pierre Van dhelsen <vandhelsen.pierre@gmail.com>
2024-12-27 00:07:11 -0500

    [Memory] remove allocator as params

--- Sources/NexusEngine/Memory/MemoryOperator.h
+++ Sources/NexusEngine/Memory/MemoryOperator.h
@@ -11,12 +11,12 @@
 
 void* operator new(std::size_t Size)
 {
-	return NxEn::Memory::Allocate(Size, NxEn::AllocatorContext::Get());
+	return NxEn::Memory::Allocate(Size);
 }
 
 void* operator new[](std::size_t Size)
 {
-	return NxEn::Memory::Allocate(Size, NxEn::AllocatorContext::Get());
+	return NxEn::Memory::Allocate(Size);
 }
 
 void* operator new(std::size_t Size, std::align_val_t Align)
@@ -57,12 +57,12 @@ void* operator new[](std::size_t Size, std::align_val_t Alignement, const std::n
 
 void operator delete(void* Pointer)
 {
-	NxEn::Memory::Free(Pointer, NxEn::AllocatorContext::Get());
+	NxEn::Memory::Free(Pointer);
 }
 
 void operator delete[](void* Pointer)
 {
-	NxEn::Memory::Free(Pointer, NxEn::AllocatorContext::Get());
+	NxEn::Memory::Free(Pointer);
 }
 
 void operator delete(void* Pointer, std::align_val_t Align)
@@ -77,12 +77,12 @@ void operator delete[](void* Pointer, std::align_val_t Align)
 
 void operator delete(void* Pointer, std::size_t Size)
 {
-	NxEn::Memory::Free(Pointer, NxEn::AllocatorContext::Get());
+	NxEn::Memory::Free(Pointer);
 }
 
 void operator delete[](void* Pointer, std::size_t Size)
 {
-	NxEn::Memory::Free(Pointer, NxEn::AllocatorContext::Get());
+	NxEn::Memory::Free(Pointer);
 }
 
 void operator delete(void* Pointer, std::size_t Size, std::align_val_t Alignement)
