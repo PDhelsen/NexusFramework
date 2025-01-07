4d41af3132d784129f6aa8da007f2f894e17cc72
Pierre Van dhelsen <vandhelsen.pierre@gmail.com>
2025-01-07 00:04:24 -0500

    [Project] set working dir as root

--- Scripts/Premake/Premake5_Engine.lua
+++ Scripts/Premake/Premake5_Engine.lua
@@ -2,6 +2,7 @@ workspace "NexusEngine"
     location "../../"
     startproject "NexusApp"
     debugcommand "../../builds/NexusApp.exe"
+	debugdir "../../"
 
     configurations { "Debug", "Release", "Ditrib" }
     platforms { "Win64" }
@@ -13,7 +14,7 @@ workspace "NexusEngine"
     filter "platforms:Win64"
         defines { "NEXUS_WINDOWS" }
         architecture "x64"
-        
+
     filter "configurations:Debug"
         defines { "NEXUS_DEBUG" }
         symbols "On"
@@ -28,7 +29,7 @@ workspace "NexusEngine"
         defines { "NEXUS_DISTRIB" }
         symbols "Off"
         optimize "On"
-        
+
     filter "toolset:msc"
         defines { "NEXUS_MSVC" }
 
@@ -48,7 +49,7 @@ group ""
 
 project "NexusEngine"
     location "../../Sources/%{prj.name}/"
-    
+
     kind "SharedLib"
     language "C++"
 	cppdialect "C++20"
@@ -76,14 +77,14 @@ project "NexusEngine"
         "NEXUS_ENGINE_DLL_BUILD"
     }
 
-    postbuildcommands 
+    postbuildcommands
     {
         "../../Scripts/PostBuildCommandsEngine.bat %{cfg.buildtarget.directory}"
     }
 
 project "NexusEditor"
     location "../../Sources/%{prj.name}/"
-    
+
     kind "SharedLib"
     language "C++"
 	cppdialect "C++20"
@@ -113,14 +114,14 @@ project "NexusEditor"
         "NexusEngine"
     }
 
-    postbuildcommands 
+    postbuildcommands
     {
         "../../Scripts/PostBuildCommandsEngine.bat %{cfg.buildtarget.directory}"
     }
 
 project "NexusApp"
     location "../../Sources/%{prj.name}/"
-    
+
     kind "ConsoleApp"
     language "C++"
 	cppdialect "C++20"
@@ -147,14 +148,14 @@ project "NexusApp"
         "NexusEditor"
     }
 
-    postbuildcommands 
+    postbuildcommands
     {
         "../../Scripts/PostBuildCommandsEngine.bat %{cfg.buildtarget.directory}"
     }
 
 project "NexusTest"
     location "../../Sources/%{prj.name}/"
-    
+
     kind "ConsoleApp"
     language "C++"
 	cppdialect "C++20"
@@ -189,15 +190,15 @@ project "NexusTest"
         "GoogleTest"
     }
 
-    postbuildcommands 
+    postbuildcommands
     {
         "../../Scripts/PostBuildCommandsEngine.bat %{cfg.buildtarget.directory}"
     }
-    
+
 
 project "GoogleTest"
     location "../../Libraries/googletest-1.14.0/"
-    
+
     kind "StaticLib"
     language "C++"
 	cppdialect "C++20"
@@ -213,9 +214,9 @@ project "GoogleTest"
         "../../Libraries/googletest-1.14.0/**.cc"
     }
 
-    removefiles 
-    { 
-        "../../Libraries/googletest-1.14.0/src/gtest-all.cc" 
+    removefiles
+    {
+        "../../Libraries/googletest-1.14.0/src/gtest-all.cc"
     }
 
     includedirs
