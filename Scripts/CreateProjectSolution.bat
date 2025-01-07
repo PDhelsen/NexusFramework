a136e004b79b09c3aa9aee2ec11ef4fdb49c63cd
Pierre Van dhelsen <vandhelsen.pierre@gmail.com>
2025-01-06 23:39:02 -0500

    [Project] Remove script relate to sandbox

--- Scripts/CreateProjectSolution.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-SET premake_file=%1
-if [%premake_file%]==[] (SET /p premake_file="Enter premake file path: ")
-
-cd Premake
-call Premake5_Project.bat %premake_file%
-
-pause
--- Scripts/PostBuildCommandsProject.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-SET source=%1
-SET destination=%source%../../builds
-SET file=%2
-
-robocopy ../../../../Softwares/NexusEngine/Install/ %destination% *.exe *.dll /it /is /v
-
-robocopy %source% %destination% %file% /it /is /e /v
-if %errorlevel% == 1 exit 0 else exit %errorlevel%
