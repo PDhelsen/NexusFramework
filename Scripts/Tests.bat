a9576401a10048afefdaa528c5425c29ff4bec77
Pierre Van dhelsen <vandhelsen.pierre@gmail.com>
2025-01-21 22:00:44 -0500

    [Scripts] Tests

--- Scripts/Tests.bat
+++ Scripts/Tests.bat
@@ -4,6 +4,8 @@ set Current=%cd%
 set Root=%~dp0..\
 cd %Root%
 call %Root%builds\artifacts\NexusTests.exe
+set Result=%errorlevel%
 cd %Current%
 
+if %Result% NEQ 0 pause
 if %errorlevel% == 0 exit /b else pause
