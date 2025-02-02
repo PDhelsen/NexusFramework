413b4759dcd8bce39eaeca0260e24fc91cecdab0
Pierre Van dhelsen <vandhelsen.pierre@gmail.com>
2025-02-01 22:02:58 -0500

    [Scripts] PostBuild

--- Scripts/Build/Steps/PostBuild.bat
+++ Scripts/Build/Steps/PostBuild.bat
@@ -1,8 +1,8 @@
 @echo off
 
 set Root=%~dp0..\..\..\
-set Binaries=%1
 set Artifacts=%Root%builds\artifacts\
+set Binaries=%1
 robocopy %Binaries% %Artifacts% *.lib *.dll *.exe /it /is /e /v
 
 if %errorlevel% NEQ 0 pause
