e0dfbc46a067ff8d19595917ad42b1d0bc47d0c0
Pierre Van dhelsen <vandhelsen.pierre@gmail.com>
2025-04-12 00:06:41 -0400

    [Appliation] Get Arguments by index

--- Sources/NexusFramework/Application/Arguments.cpp
+++ Sources/NexusFramework/Application/Arguments.cpp
@@ -5,60 +5,37 @@ namespace NxFr
 {
 	namespace Arguments
 	{
-		static Dictionary<StringView, StringView>& GetArgs() { static Dictionary<StringView, StringView> Args(1, nullptr); return Args; }
+		static List<StringView>& GetArgs() { static List<StringView> Args(1, nullptr); return Args; }
+		static Dictionary<StringView, StringView>& GetArgsProcessed() { static Dictionary<StringView, StringView> Args(1, nullptr); return Args; }
 
 		void Parse(uint64 ArgC, char* ArgV[])
 		{
-			Dictionary<StringView, StringView>& Args = GetArgs();
+			List<StringView>& Args = GetArgs();
+			Dictionary<StringView, StringView>& ArgsProcessed = GetArgsProcessed();
 			NEXUS_ASSERT(Args.GetCount() == 0, Default, "Arguments were already parsed");
 
-			Args.Grow(Math::NextPrime(ArgC));
+			Args.Grow(ArgC);
+			ArgsProcessed.Grow(Math::NextPrime(ArgC));
 
-			Args.Append(KeyProgram, ArgV[0]);
+			Args.Append(ArgV[0]);
+			ArgsProcessed.Append(KeyProgram, ArgV[0]);
+
 			for (uint64 Index = 1; Index < ArgC; ++Index)
 			{
 				StringView Arg = ArgV[Index];
 
 				List<StringView> KeyValue = Arg.SplitAll("=");
 				StringView Key = KeyValue[0];
-				StringView Value = KeyValue.GetCount() == 2 ? KeyValue[1] : "True";
+				StringView Value = KeyValue.GetCount() == 2 ? KeyValue[1] : "";
 
-				Args.Append(Key, Value);
+				Args.Append(ArgV[Index]);
+				ArgsProcessed.Append(Key, Value);
 			}
-		}
-
-		bool HasFlag(StringView Key, bool Default)
-		{
-			Dictionary<StringView, StringView>& Args = GetArgs();
-			bool Contain = Args.ContainsKey(Key);
-			return Contain ? true : Default;
-		}
-
-		StringView GetValue(StringView Key, StringView Default)
-		{
-			Dictionary<StringView, StringView>& Args = GetArgs();
-			StringView* Contain = Args.TryGet(Key);
-			return Contain != nullptr ? *Contain: Default;
-		}
-
-		Dictionary<StringView, StringView>::I Begin()
-		{
-			return GetArgs().Begin();
-		}
-
-		Dictionary<StringView, StringView>::I End()
-		{
-			return GetArgs().End();
-		}
-
-		int64 GetCount()
-		{
-			return GetArgs().GetCount();
 		}
 
 		void Log()
 		{
-			Dictionary<StringView, StringView>& Args = GetArgs();
+			Dictionary<StringView, StringView>& Args = GetArgsProcessed();
 			NEXUS_LOG(Info, Default, "Arguments count: %d", Args.GetCount());
 
 			uint64 Index = 0;
@@ -66,6 +43,41 @@ namespace NxFr
 			{
 				NEXUS_LOG(Info, Default, "Argument %d: %s = %s", Index, Iter->Key.ToString().C(), Iter->Value.ToString().C());
 			}
+		}
+
+		StringView GetValue(uint64 Index, StringView Default)
+		{
+			List<StringView>& Args = GetArgs();
+			return Args[Index];
+		}
+
+		StringView GetValue(StringView Key, StringView Default)
+		{
+			Dictionary<StringView, StringView>& Args = GetArgsProcessed();
+			StringView* Contain = Args.TryGet(Key);
+			return Contain != nullptr ? *Contain : Default;
+		}
+
+		bool HasFlag(StringView Key, bool Default)
+		{
+			Dictionary<StringView, StringView>& Args = GetArgsProcessed();
+			bool Contain = Args.ContainsKey(Key);
+			return Contain ? true : Default;
+		}
+
+		Dictionary<StringView, StringView>::I Begin()
+		{
+			return GetArgsProcessed().Begin();
+		}
+
+		Dictionary<StringView, StringView>::I End()
+		{
+			return GetArgsProcessed().End();
+		}
+
+		int64 GetCount()
+		{
+			return GetArgsProcessed().GetCount();
 		}
 	}
 }
--- Sources/NexusFramework/Application/Arguments.h
+++ Sources/NexusFramework/Application/Arguments.h
@@ -11,14 +11,14 @@ namespace NxFr
 		inline static const StringView KeyProgram = "Program";
 
 		NEXUS_FRAMEWORK_API void Parse(uint64 ArgC, char* ArgV[]);
+		NEXUS_FRAMEWORK_API void Log();
 
-		NEXUS_FRAMEWORK_API bool HasFlag(StringView Key, bool Default = false);
+		NEXUS_FRAMEWORK_API StringView GetValue(uint64 Index, StringView Default = "");
 		NEXUS_FRAMEWORK_API StringView GetValue(StringView Key, StringView Default = "");
+		NEXUS_FRAMEWORK_API bool HasFlag(StringView Key, bool Default = false);
 
 		NEXUS_FRAMEWORK_API Dictionary<StringView, StringView>::I Begin();
 		NEXUS_FRAMEWORK_API Dictionary<StringView, StringView>::I End();
 		NEXUS_FRAMEWORK_API int64 GetCount();
-
-		NEXUS_FRAMEWORK_API void Log();
 	}
 }
